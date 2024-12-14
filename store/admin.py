79ca2f6b06ffde1dedc3a23125741e89672e8f97
ankitkarki15 <ankitkarki8088@gmail.com>
2024-12-14 18:42:10 +0545

    Add .gitignore and stop tracking ignored files

--- store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import*
-from .models import Category,Customer, Product , Order
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
