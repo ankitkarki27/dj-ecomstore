41526e124824587778a4e6975c6608d68a566e57
ankitkarki27 <ankitkarki8088@gmail.com>
2025-11-18 12:45:50 +0545

    completed

--- ecomstore/urls.py
+++ ecomstore/urls.py
@@ -2,15 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from . import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-
-def healthz(request):
-    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('healthz', healthz),
-   
     path('', include('store.urls')),    
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
